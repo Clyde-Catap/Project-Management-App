dadf070a38d5a1dffd7cf15c4ac9d2ae8127bf70
Dev-Clyde <dev-clyde@Dev-Environment.myguest.virtualbox.org>
2024-05-28 05:05:32 +0800

    initial content

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,17 @@ import React from "react";
 
 
 import YourProjectsSideBar from "./YourProjectsSidebar";
-
+import Content from "./Content";
 
 function App() {
-
+  const classNameMainDiv = "flex"
 
   return (
-    <>
+    <div className={classNameMainDiv}>        
+      <YourProjectsSideBar/>
+      <Content/>
 
-        <YourProjectsSideBar/>
-
-
-
-    </>
+    </div>
   );
 }
 
