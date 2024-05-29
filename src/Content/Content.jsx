f5908abbd514aefb91385b64131ddbcd75f0c690
Dev-Clyde <dev-clyde@Dev-Environment.myguest.virtualbox.org>
2024-05-29 20:05:58 +0800

    add styling to Title

--- src/Content/Content.jsx
+++ src/Content/Content.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import Default from "./Default";
 import Editor from "./Editor";
+import Title from "./Editor";
 
 export default function Content(){
 
-    const classNameContentContainer = "flex flex-col items-center justify-center w-full";
+    const classNameContentContainer = "flex flex-col items-left justify-center w-full";
+    const classNameContentInnerContainer = "h-9/12 w-9/12 p-10";
+
     const def = false;
     let comp;
 
     if (def){
        comp =  <Default></Default>
     } else {
-        comp = <Editor></Editor>
+        comp = <Title></Title>
     }
 
 
@@ -19,7 +22,9 @@ export default function Content(){
 
     return (
     <div className={classNameContentContainer}>
-        {comp}
+        <div className={classNameContentInnerContainer}>
+            {comp}
+        </div>
     </div>
     );
 }
--- src/Content/Editor.jsx
+++ src/Content/Editor.jsx
@@ -1,36 +1,42 @@
 import React from "react";
 
-export default function Editor (){
-    const classNameButtonContainer = "";
+export default function Title (){
+
+    const classNameText = "text-stone-500 font-bold";
+    const classNameInputContainer = "p-5";
+
+    const classNameButtonContainer = "grid justify-items-end pr-5";
     const classNameSaveButton = "px-2 py-2 bg-black text-white rounded-md pl-5 pr-5";
     const classNameCancelButton = "m-5";
-    const classNameText = "text-stone-500 font-bold";
 
-    const classNameTitleContainer = "";
-    const classNameTitleInput = "bg-stone-300";
 
-    const classNameDescriptionContainer = "";
-    const classNameDescriptionInput = "bg-stone-300";
+    // const classNameTitleContainer = "p-5";
+    const classNameTitleInput = "bg-stone-300 h-10 w-full";
+
+    // const classNameDescriptionContainer = "p-5";
+    const classNameDescriptionInput = "bg-stone-300 h-20 w-full";
     
-    const classNameDueDateContainer = "";
-    const classnameDueDateInput = "bg-stone-300";
+    // const classNameDueDateContainer = "p-5";
+    const classnameDueDateInput = "bg-stone-300 h-10 w-full";
 
 
     return (
     <>
         <div className={classNameButtonContainer}>
-            <button className={classNameCancelButton}>Cancel</button>
-            <button className={classNameSaveButton}>Save</button>
+            <div>
+                <button className={classNameCancelButton}>Cancel</button>
+                <button className={classNameSaveButton}>Save</button>
+            </div>
         </div>
-        <div className={classNameTitleContainer}>
+        <div className={classNameInputContainer}>
             <h2 className={classNameText}>TITLE</h2>
             <input className={classNameTitleInput}></input>
         </div>
-        <div className={classNameDescriptionContainer}>
+        <div className={classNameInputContainer}>
             <h2 className={classNameText}>DESCRIPTION</h2>
             <input className={classNameDescriptionInput}></input>
         </div>
-        <div className={classNameDueDateContainer}>
+        <div className={classNameInputContainer}>
             <h2 className={classNameText}>DUE DATE</h2>
             <input type='date' className={classnameDueDateInput}></input>
         </div>
